31dcabb003990465f24dca420ecdaa9a55e9588b
ym <ymmanyuan@outlook.com>
2025-04-16 14:49:11 +0800

    small bug fixed: use MPI_SUM instead of MPI_MAX when calculating TotalRemoved cells in Comm 	modified:   src/geometry/block_geometry3d.hh

--- src/geometry/block_geometry3d.hh
+++ src/geometry/block_geometry3d.hh
@@ -281,6 +281,7 @@ template <typename FieldType>
 void BlockGeometry3D<T>::InitVoxelMap(FieldType& FieldM, std::uint8_t voidflag) {
   #ifdef _VOX_ENABLED
   std::size_t TotalRemoved{};
+  std::size_t TotalvoxNum{};
   int iblock{};
   for (Block3D<T>& block : _Blocks) {
     auto& field = FieldM.getBlockField(iblock).getFieldType().getField(0);
@@ -295,6 +296,7 @@ void BlockGeometry3D<T>::InitVoxelMap(FieldType& FieldM, std::uint8_t voidflag) 
         ++voxNum;
       }
     }
+    TotalvoxNum += voxNum;
 
     block.getVoxelMap().Init(voxNum);
     std::size_t* vox_aabb = block.getVoxelMap().getVox_to_AABB();
@@ -430,12 +432,14 @@ void BlockGeometry3D<T>::InitVoxelMap(FieldType& FieldM, std::uint8_t voidflag) 
   }
 
   #ifdef MPI_ENABLED
-		mpi().reduceAndBcast(TotalRemoved, MPI_MAX);
+    mpi().reduceAndBcast(TotalRemoved, MPI_SUM);
+    mpi().reduceAndBcast(TotalvoxNum, MPI_SUM);
   #endif
 
   MPI_RANK(0)
   std::cout << "[BlockGeometry::InitVoxelMap]:\n"
-              << "  Removed: " << TotalRemoved << " cell pairs in Communicator" << std::endl;
+            << "  Total voxel number: " << TotalvoxNum << "\n"
+            << "  Removed: " << TotalRemoved << " cell pairs in Communicator" << std::endl;
   
   #endif
 }
